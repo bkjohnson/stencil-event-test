269a4a291ac1e88ce872642c4d028e8e5f6120e1
Brooks Johnson <brooks.johnson@pm.me>
2021-07-20 09:38:13 -0700

    Shift some things around

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import 'web-components/dist/component-library/component-library.css';
-
-import { defineCustomElements } from 'web-components/loader';
-defineCustomElements();
-
-
 function App() {
+  const onClose = (e) => console.log('CLOSING', e)
+  console.log('hi;')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <va-alert onFoo={() => console.log('foo')} onClose={() => console.log('closing')} closeable>
+    <div>
+      <va-alert onFoo={() => console.log('foo')} onClose={onClose} closeable>
+        <h3 slot="headline">This is an alert</h3>
           Foo
         </va-alert>
-
-      </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'web-components/dist/component-library/component-library.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { defineCustomElements } from 'web-components/loader';
+defineCustomElements();
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
